c92e51d9bc6ec6b5dc3d18646182e8f1adb1c8b8
Sergey <52304443+c29r3@users.noreply.github.com>
2021-04-16 17:40:20 +0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+systemctl stop ag-chain-cosmos.service \
+&& cd ~/agoric \
+rm -rf agoric-sdk; \
+&& git clone https://github.com/Agoric/agoric-sdk -b agorictest-9 \
+&& cd agoric-sdk \
+&& yarn install \
+&& yarn build \
+&& cd packages/cosmic-swingset \
+&& make \
+&& ag-chain-cosmos version \
+&& cd ~/agoric \
+&& curl https://testnet.agoric.net/network-config > chain.json \
+&& chainName=`jq -r .chainName < chain.json` \
+&& curl https://testnet.agoric.net/genesis.json > $HOME/.ag-chain-cosmos/config/genesis.json \
+&& ag-chain-cosmos unsafe-reset-all \
+&& peers=$(jq '.peers | join(",")' < chain.json) \
+&& seeds=$(jq '.seeds | join(",")' < chain.json) \
+&& sed -i.bak 's/^log_level/# log_level/' $HOME/.ag-chain-cosmos/config/config.toml \
+&& sed -i.bak -e "s/^seeds *=.*/seeds = $seeds/; s/^persistent_peers *=.*/persistent_peers = $peers/" $HOME/.ag-chain-cosmos/config/config.toml \
+&& systemctl start ag-chain-cosmos.service \
+&& journalctl -u ag-chain-cosmos.service -f --no-hostname
