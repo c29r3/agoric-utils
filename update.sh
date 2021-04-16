cf2b6f59eacf11283fe23c77140794e366a4dc62
Sergey <52304443+c29r3@users.noreply.github.com>
2021-04-16 22:32:54 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ systemctl stop ag-chain-cosmos.service \
 && rm -rf agoric-sdk; \
 git clone https://github.com/Agoric/agoric-sdk -b agorictest-9 \
 && cd agoric-sdk \
+&& yarn cache clean \
 && yarn install \
 && yarn build \
 && cd packages/cosmic-swingset \
@@ -12,12 +13,11 @@ git clone https://github.com/Agoric/agoric-sdk -b agorictest-9 \
 && ag-chain-cosmos version \
 && cd ~/agoric \
 && curl https://testnet.agoric.net/network-config > chain.json \
-&& chainName=`jq -r .chainName < chain.json` \
-&& curl https://testnet.agoric.net/genesis.json > $HOME/.ag-chain-cosmos/config/genesis.json \
+&& chainName=`jq -r .chainName < chain` \
 && ag-chain-cosmos unsafe-reset-all \
+&& curl http://157.245.11.32:26657/genesis.json > $HOME/.ag-chain-cosmos/config/genesis.json \
 && peers=$(jq '.peers | join(",")' < chain.json) \
 && seeds=$(jq '.seeds | join(",")' < chain.json) \
-&& sed -i.bak 's/^log_level/# log_level/' $HOME/.ag-chain-cosmos/config/config.toml \
 && sed -i.bak -e "s/^seeds *=.*/seeds = $seeds/; s/^persistent_peers *=.*/persistent_peers = $peers/" $HOME/.ag-chain-cosmos/config/config.toml \
 && systemctl start ag-chain-cosmos.service \
 && journalctl -u ag-chain-cosmos.service -f --no-hostname
